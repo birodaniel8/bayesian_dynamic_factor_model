407b180fc255b996e38d009ffb5a4da8ea1f00e0
Daniel Biro <birodaniel8@gmail.com>
2020-04-11 18:50:53 +0200

    normal linear regression estimation added

---
+++ estimate_normal_linear_regression.m
@@ -0,0 +1,32 @@
+function [sampled_beta, sampled_sigma_squared] = estimate_normal_linear_regression(y,x,priors,ndraw,burnin,constant,display)
+% ESTIMATE_NORMAL_LINEAR_REGRESSION
+
+    if isfield(priors,'beta'), beta_prior = priors.beta; else beta_prior = 0; end  % prior mean of the beta coefficients in the normal model
+    if isfield(priors,'V'), V_prior = priors.V; else V_prior = 1; end  % prior variance of the beta coefficients in the normal model
+    if isfield(priors,'gamma'), gamma_prior = priors.gamma; else gamma_prior = 3/2; end  % prior confidence in the variance in the normal model
+    if isfield(priors,'delta'), delta_prior = priors.delta; else delta_prior = 0.01/2; end  % prior mean of the variance in the normal model
+    
+    [T, m] = size(x);
+    if constant
+        x = [ones(T,1) x];
+        m = m + 1;
+    end
+    
+    % create containers:
+    sampled_beta = zeros(m, ndraw-burnin);
+    sampled_sigma_squared = zeros(1, ndraw-burnin);
+    
+    % initial values:
+    sigma_squared = delta_prior;
+
+    % sampling:
+    for i = 1:ndraw
+        if display && mod(i,100) == 0, disp(i); end
+        beta = sampling_beta(x, y, beta_prior, V_prior, sigma_squared);   % sampling beta coefficients
+        sigma_squared = sampling_sigma_squared(y-x*beta, gamma_prior, delta_prior);  % sampling error variance
+        if i > burnin
+            sampled_beta(:, i-burnin) = beta;
+            sampled_sigma_squared(:, i-burnin) = sigma_squared;
+        end
+    end
+end
